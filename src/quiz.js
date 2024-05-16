130984cffe9e67abd3ab3768f4108d52cfd64583
Kei Ikushima <z5430271@ad.unsw.edu.au>
2024-05-16 18:18:12 +1000

    Added function adminQuizInfo

--- src/quiz.js
+++ src/quiz.js
@@ -10,6 +10,16 @@ function adminQuizRemove( authUserId, name, description ) {
     }
 }
 
+function adminQuizInfo( authUserId, quizId ) {
+    return {
+        quizId: 1,
+        name: 'My Quiz',
+        timeCreated: 1683125870,
+        timeLastEdited: 1683125871,
+        description: 'This is my quiz',
+    }
+}
+
 function adminQuizNameUpdate(authUserId, quizId, name) {
     return {}
 }

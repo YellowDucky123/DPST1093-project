8661e152dfae695b79c85f1edeea72333d73b3de
Kei Ikushima <z5430271@ad.unsw.edu.au>
2024-06-02 13:41:15 +1000

    Updated test

--- tests/adminQuizInfo.test.js
+++ tests/adminQuizInfo.test.js
@@ -7,15 +7,69 @@ beforeEach(() => {
   });
   describe('adminQuizInfo Tests', () => {
     test('invalid user id', () => {
-      adminQuizInfo("1", '2');
-      expect(adminQuizInfo("1", '2')).toStrictEqual({error: 'adminQuizInfo: invalid user id'});
+        let data = getData();
+        data.quizzes = {
+            '321': {
+                name: 'thisIsName'
+            },
+            '111': {
+                name: 'hello'
+            }
+        }
+        data.users = {
+            '123': {
+                authUserId: 123,
+                quizzesUserHave : ['321']
+            },
+            '124': {
+                authUserId: 124,
+                quizzesUserHave : ['111']
+            }
+        };
+        setData(data);
+
+      expect(adminQuizInfo('125', '111')).toStrictEqual({error: 'adminQuizInfo: invalid user id'});
     });
     test('invalid quiz id', () => {
-        adminQuizInfo("1", '2');
-        expect(adminQuizInfo("1", '2')).toStrictEqual({error: 'adminQuizInfo: invalid quiz id'});
+        let data = getData();
+        data.quizzes = {
+            '321': {
+                name: 'thisIsName'
+            }
+        }
+        data.users = {
+            '123': {
+                authUserId: 123,
+                quizzesUserHave : ['321']
+            },
+            '124': {
+                authUserId: 124,
+                quizzesUserHave : []
+            }
+        };
+        setData(data);
+
+        expect(adminQuizInfo('123', '2')).toStrictEqual({error: 'adminQuizInfo: invalid quiz id'});
     });
     test('do not own this', () => {
-        adminQuizInfo("1", '2');
-        expect(adminQuizInfo("1", '2')).toStrictEqual({error: 'adminQuizInfo: you do not own this quiz'});
+        let data = getData();
+        data.quizzes = {
+            '321': {
+                name: 'thisIsName'
+            }
+        }
+        data.users = {
+            '123': {
+                authUserId: 123,
+                quizzesUserHave : ['321']
+            },
+            '124': {
+                authUserId: 124,
+                quizzesUserHave : []
+            }
+        };
+        setData(data);
+
+        expect(adminQuizInfo('124', '321')).toStrictEqual({error: 'adminQuizInfo: you do not own this quiz'});
     });
 });
--- tests/adminQuizRemove.test.js
+++ tests/adminQuizRemove.test.js
@@ -7,15 +7,53 @@ beforeEach(() => {
   });
   describe('adminQuizRemove Tests', () => {
     test('invalid user id', () => {
-      adminQuizRemove("1", '2');
-      expect(adminQuizRemove("1", '2')).toStrictEqual({error: 'adminQuizRemove: invalid user id'});
+        let data = getData();
+        data.quizzes = {
+            '321': {
+                name: 'thisIsName'
+            }
+        }
+        data.users = {
+            '123': {
+                authUserId: 123
+            }
+        };
+        setData(data);
+      expect(adminQuizRemove('124', '321')).toStrictEqual({error: 'adminQuizRemove: invalid user id'});
     });
     test('invalid quiz id', () => {
-        adminQuizRemove("1", '2');
-        expect(adminQuizRemove("1", '2')).toStrictEqual({error: 'adminQuizRemove: invalid quiz id'});
+        let data = getData();
+        data.quizzes = {
+            '1': {
+                name: 'thisIsName'
+            }
+        }
+        data.users = {
+            '123': {
+                authUserId: 123
+            }
+        };
+        setData(data);
+        expect(adminQuizRemove('123', '2')).toStrictEqual({error: 'adminQuizRemove: invalid quiz id'});
     });
     test('do not own this', () => {
-        adminQuizRemove("1", '2');
-        expect(adminQuizRemove("1", '2')).toStrictEqual({error: 'adminQuizRemove: you do not own this quiz'});
+        let data = getData();
+        data.quizzes = {
+            '321': {
+                name: 'thisIsName'
+            }
+        }
+        data.users = {
+            '123': {
+                authUserId: 123,
+                quizzesUserHave : ['321']
+            },
+            '124': {
+                authUserId: 124,
+                quizzesUserHave : []
+            }
+        };
+        setData(data);
+        expect(adminQuizRemove('124', '321')).toStrictEqual({error: 'adminQuizRemove: you do not own this quiz'});
     });
 });

9aefd65697ebd8638c5084093230808a89a2fd8c
Yuxuan Wang <z5487045@ad.unsw.edu.au>
2024-06-02 13:11:24 +1000

    I finished function adminUserPasswordUpdate in file<src/auth.js> and its test function

---
+++ tests/adminQuizList.test.js
@@ -0,0 +1,92 @@
+import { setData, getData } from "../src/dataStore";
+import { adminQuizList } from "../src/quiz";
+
+setData({
+  users: {
+    111: {
+      name: "test",
+      authUserId: 111,
+      email: "aofaij@???.com",
+      password: "1111",
+      numSuccessfulLogin: 1,
+      numFailedPasswordsSinceLastLogin: 0,
+      quizzesUserHave: [111, 222, 333]
+    },
+    222: {
+      name: "jest",
+      authUserId: 222,
+      email: "wq;pqoergn@???.com",
+      password: "2222",
+      numSuccessfulLogin: 1,
+      numFailedPasswordsSinceLastLogin: 0,
+      quizzesUserHave: []
+    },
+    444 : {
+      name: "jest",
+      authUserId: 444,
+      email: "wq;woejgergn@???.com",
+      password: "2222",
+      numSuccessfulLogin: 1,
+      numFailedPasswordsSinceLastLogin: 0,
+      quizzesUserHave: [111, 222]
+    }
+  },
+  quizzes: {
+    111: {
+      quizId: 111,
+      name: "114514",
+      timeCreated: 11,
+      timeLastEdited: 11
+    },
+    222: {
+      quizId: 222,
+      name: "wpigjj",
+      timeCreated: 11,
+      timeLastEdited: 11
+    },
+    333: {
+      quizId: 333,
+      name: "wjldbv",
+      timeCreated: 11,
+      timeLastEdited: 11
+    }
+  }
+})
+
+test("no such a user", () => {
+  expect(adminQuizList(333)).toEqual({ error: "can not find such a member" })
+})
+
+test("quizzes for the one have no quiz", () => {
+  expect(adminQuizList(222)).toEqual({ quizzes: [] });
+})
+test("quizzes for the one have lot of quizzes", () => {
+  expect(adminQuizList(111)).toEqual({
+    quizzes: [{
+      name: "114514",
+      quizId: 111
+    },
+    {
+      name: "wpigjj",
+      quizId: 222
+    },
+    {
+      name: "wjldbv",
+      quizId: 333
+    }
+    ]
+  });
+})
+test("quizzes for the one have some quizzes", () => {
+  expect(adminQuizList(444)).toEqual({
+    quizzes: [{
+      name: "114514",
+      quizId: 111
+    },
+    {
+      name: "wpigjj",
+      quizId: 222
+    }
+    ]
+  });
+})
---
+++ tests/adminUserPasswordUpdate.test.js
@@ -0,0 +1,26 @@
+import { adminAuthRegister, adminUserPasswordUpdate } from "../src/auth";
+
+let idForCommonOne = adminAuthRegister("commonMan@163.com", "Suitable123", "common", "human")
+
+test("Change password correctly", () => {
+  expect(adminUserPasswordUpdate(idForCommonOne.authUserId, "Suitable123", "Suitable456")).toEqual({});
+})
+
+test("new password not suitable", () => {
+  expect(adminUserPasswordUpdate(idForCommonOne.authUserId, "Suitable456", "a111111")).toEqual({ error: 'Password should be at least than 8 characters' })
+  expect(adminUserPasswordUpdate(idForCommonOne.authUserId, "Suitable456", "1111111111111111111111")).toEqual({ error: "Password should contain at least one number and at least one letter" })
+  expect(adminUserPasswordUpdate(idForCommonOne.authUserId, "Suitable456", "aaaaaaaaaaaaaaaa")).toEqual({ error: "Password should contain at least one number and at least one letter" })
+})
+
+test("change password by a wrong password", () => {
+  expect(adminUserPasswordUpdate(idForCommonOne.authUserId, "Suitable123", "Suitable456")).toEqual({ error: "password incorrecrt" });
+})
+
+test("change password by a used password", () => {
+  expect(adminUserPasswordUpdate(idForCommonOne.authUserId, "Suitable456", "Suitable123")).toEqual({ error: "This password has been used in past" })
+})
+
+
+test("change password by the password right now", () => {
+  expect(adminUserPasswordUpdate(idForCommonOne.authUserId, "Suitable456", "Suitable456")).toEqual({ error: "new Password can't be the old password" })
+})

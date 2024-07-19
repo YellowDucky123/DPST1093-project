8f71c89bb76f10a91e5493c0e179e6e423506c97
Kelvin Jonathan Yoga <kelvinjyoga@gmail.com>
2024-07-19 11:18:05 +1000

    fixed some tests

--- src/http-tests/sendChat.test.ts
+++ src/http-tests/sendChat.test.ts
@@ -5,6 +5,8 @@ import { error } from 'console';
 import { requestHelper} from './requestHelper_fn'
 import HTTPError from 'http-errors';
 import { send } from 'process';
+import { create } from 'ts-node';
+import { before } from 'node:test';
 
 const port = config.port;
 const url = config.url;
@@ -12,39 +14,94 @@ const url = config.url;
 const SERVER_URL = `${url}:${port}`;
 
 afterAll(() => {
-  request(
-    'DELETE', SERVER_URL + '/v1/clear'
-  );
+    requestHelper('DELETE', '/v2/clear', {});
 });
 
-function startSession(quizId: number, autoStartNum: number, token) {
-    return requestHelper('POST', `/v1/admin/quiz/${quizId}/sessions/start`, {autoStartNum}, token);
+beforeAll(() => {
+    requestHelper('DELETE', '/v2/clear', {});
+});
+
+requestHelper('DELETE', '/v2/clear', {});
+
+function authRegister(email: string, password: string, nameFirst: string, nameLast: string) {
+    return requestHelper('POST', '/v1/admin/auth/register', {email, password, nameFirst, nameLast});
 }
 
-function sendChat(message: string, playerId: number, token) {
-    return requestHelper('POST', `/v1/player/${playerId}/chat`, {message}, token);
+function loginUser(email: string, password: string) {
+    return requestHelper('POST', '/v1/admin/auth/login', {email, password});
+}
+function createQuiz(token: number, name: string, description: string) {
+    return requestHelper('POST', '/v1/admin/quiz', {token, name, description});
 }
 
+function createQuestion(quizId: number, token: string) {
+    return requestHelper('POST', `/v2/admin/quiz/${quizId}/question`, {
+        "questionBody": {
+          "question": "Who is the Monarch of England?",
+          "duration": 4,
+          "points": 5,
+          "answers": [
+            {
+              "answer": "Prince Charles",
+              "correct": true
+            },
+            {
+                answer: 'Queen Bels',
+                correct: false
+            }
+          ],
+          "thumbnailUrl": "http://google.com/some/image/path.jpg"
+        }
+      }, {token});
+}
 
+function startSession(quizId: number, autoStartNum: number, token: string) {
+    return requestHelper('POST', `/v1/admin/quiz/${quizId}/sessions/start`, {autoStartNum}, {token});
+}
+
+function joinSession(sessionId: number, userName: string) {
+    return requestHelper('POST', '/v1/player/join', {sessionId, userName}, {});
+}
+
+function sendChat(message: string, playerId: number, token:string) {
+    return requestHelper('POST', `/v1/player/${playerId}/chat`, {message}, {token});
+}
+
+const authToken = authRegister(
+    'kelvinjyoga@gmail.com',
+    'password123@!',
+    'Kelvin',
+    'Yoga'
+);
+
+const Token = loginUser('kelvinjyoga@gmail.com', 'password123@!');
+const userToken = Token.token;
+
+const qzId = createQuiz(userToken, 'question1', 'this is a test q');
+const quizId = qzId.quizId;
+createQuestion(quizId, userToken);
+
+const sessionId = startSession(quizId, 3, userToken);
+const playerId = joinSession(sessionId.sessionId, 'player');
 
 describe('error', () => {
   test('player does not exist', () => {
-    expect(() => sendChat('Hello Everyone', playerId, token)).toThrow(HTTPError[400]);
+    expect(() => sendChat('Hello Everyone', playerId, userToken)).toThrow(HTTPError[400]);
   });
 
   test('message too Long', () => {
     let str: string = '';
     str = String(str).padStart(110, '*');
-    expect(() => sendChat(str, )).toThrow(HTTPError[400]);
+    expect(() => sendChat(str, playerId, userToken)).toThrow(HTTPError[400]);
   });
 
   test('message too short', () => {
     let str: string = '';
-    expect(() => sendChat(str, )).toThrow(HTTPError[400]);
+    expect(() => sendChat(str, playerId, userToken)).toThrow(HTTPError[400]);
   });
 });
 
 test('succesfull send', () => {
-    expect(() => sendChat(str, )).toEqual({});
+    expect(() => sendChat('Greeting Earthlings!', playerId, userToken)).toEqual({});
 });
 
